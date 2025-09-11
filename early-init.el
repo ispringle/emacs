73758b315927d960168b326817c3d286b736233f
Ian S. Pringle <ian@dapringles.com>
2025-09-11 13:33:48 -0500

    supress lexical binding warnings, I think a v31 bug is spamming it

--- early-init.el
+++ early-init.el
@@ -11,7 +11,7 @@
 
 ;; Be a little quieter
 (setq byte-compile-warnings '(not obsolete))
-(setq warning-suppress-log-types '((comp) (bytecomp)))
+(setq warning-suppress-log-types '((comp) (bytecomp) (files missing-lexbind-cookie)))
 (setq native-comp-async-report-warnings-errors 'silent)
 (setq inhibit-startup-echo-area-message (user-login-name))
 
